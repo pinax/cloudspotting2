f4204412121aa1b87558fe0d0ef3acc60b8f7b06
Graham Ullrich <graham@flyingcracker.com>
2018-01-31 20:50:27 -0700

    Add setup.py for versioning

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudspotting2",
+    version="1.0",
+    packages=find_packages(),
+    scripts=["manage.py"]
+)
