30889b33d8b3334ed1af6a5b9e07c3015212e813
Graham Ullrich <graham@flyingcracker.com>
2018-01-31 20:51:26 -0700

    semver version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloudspotting2",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
     scripts=["manage.py"]
 )
